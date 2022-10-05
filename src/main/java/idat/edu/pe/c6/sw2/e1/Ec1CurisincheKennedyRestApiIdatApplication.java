7f10c91cc06d57e33396305e07daf1905fa06c8c
RoggerK <at76324897@idat.edu.pe>
2022-10-04 19:23:28 -0500

    subiendo a git

---
+++ src/main/java/idat/edu/pe/c6/sw2/e1/Ec1CurisincheKennedyRestApiIdatApplication.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.c6.sw2.e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec1CurisincheKennedyRestApiIdatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec1CurisincheKennedyRestApiIdatApplication.class, args);
+	}
+
+}
